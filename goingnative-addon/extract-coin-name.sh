8117a5f35c24194ffeed0622200731a9ed57ffae
Gerry Agbobada <gerry.agbobada-ext@ledger.fr>
2020-06-01 14:35:30 +0200

    Fix extract coin name script

--- goingnative-addon/extract-coin-name.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-command -v git >/dev/null 2>&1 || (echo "Please install git" && exit 1)
-
-branch_name="$(git rev-parse --abbrev-ref HEAD)"
-
-echo "${branch_name#*-}" # remove prefix ending in "-"
